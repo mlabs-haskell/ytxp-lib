4ab3e56d8d31552ccd1a110bc6dc964ff73a1dc7
Alberto Fanton <alberto.fanton@protonmail.com>
2024-04-23 01:11:31 +0200

    Fix minimal nix shell

--- yielding-transaction-lib/plutarch/flake.nix
+++ yielding-transaction-lib/plutarch/flake.nix
@@ -13,6 +13,7 @@
     CHaP = {
       url = "github:intersectmbo/cardano-haskell-packages?ref=repo";
     };
+    liqwid-plutarch-extra.url = "github:Liqwid-Labs/liqwid-plutarch-extra";
   };
 
   outputs = inputs@{ nixpkgs, haskellNix, flake-parts, iohk-nix, CHaP, plutarch, ... }:
