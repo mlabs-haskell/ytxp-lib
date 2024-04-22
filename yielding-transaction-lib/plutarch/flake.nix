011c2a272beda84fbc74e27f636433c0ebe2c994
Alberto Fanton <alberto.fanton@protonmail.com>
2024-04-22 16:14:27 +0200

    Factorize files

--- yielding-transaction-lib/plutarch/flake.nix
+++ yielding-transaction-lib/plutarch/flake.nix
@@ -4,11 +4,14 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskellNix.url = "github:input-output-hk/haskell.nix";
     iohk-nix.url = "github:input-output-hk/iohk-nix";
-    nixpkgs.follows = "iohk-nix/nixpkgs";
-    plutarch.url = "github:Plutonomicon/plutarch-plutus";
+    nixpkgs.follows = "haskellNix/nixpkgs";
+    plutarch = {
+      url = "github:Plutonomicon/plutarch-plutus";
+      flake = false;
+    };
+
     CHaP = {
       url = "github:intersectmbo/cardano-haskell-packages?ref=repo";
-      flake = false;
     };
   };
 
--- yielding-transaction-lib/plutarch/nix/project.nix
+++ yielding-transaction-lib/plutarch/nix/project.nix
@@ -1,7 +1,6 @@
 { pkgs, lib, inputs }:
 pkgs.haskell-nix.cabalProject'
 {
-  sha256map = { "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg="; };
   compiler-nix-name = "ghc963";
   src = lib.cleanSource ./../ytxp-plutarch;
   shell = {
@@ -16,4 +15,6 @@ pkgs.haskell-nix.cabalProject'
     "https://chap.intersectmbo.org/" = inputs.CHaP;
     "https://github.com/Plutonomicon/plutarch-plutus.git" = inputs.plutarch;
   };
+  sha256map = { "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg="; };
+
 }
