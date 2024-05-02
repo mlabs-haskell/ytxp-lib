4bc4e06a1e6deaba6f092afa633fcd9da89545c0
Alberto Fanton <alberto.fanton@protonmail.com>
2024-05-02 10:20:35 +0200

    Remove hlint config file and update data->newtype

--- yielding-transaction-lib/plutarch/ytxp-plutarch/src/Cardano/YTxP/Control/YieldList.hs
+++ yielding-transaction-lib/plutarch/ytxp-plutarch/src/Cardano/YTxP/Control/YieldList.hs
@@ -204,7 +204,7 @@ deriving via
 The length of the datum is checked upon creation in `mkYieldListSTMPWrapper` to ensure
 that the length of the list does not exceed the max list length passed as a parameter to that script.
 -}
-data YieldListDatum = YieldListDatum
+newtype YieldListDatum = YieldListDatum
   { yieldedToScripts :: [YieldedToHash]
   }
   deriving stock (Show, Eq, Generic)
