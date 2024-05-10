af0b58a7c94fd71b1e70c68f031528a0f8ca143a
Alberto Fanton <alberto.fanton@protonmail.com>
2024-05-10 14:41:40 +0200

    Fix format

--- yielding-transaction-lib/plutarch/ytxp-plutarch/app/write-config/Main.hs
+++ yielding-transaction-lib/plutarch/ytxp-plutarch/app/write-config/Main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
 import Cardano.YTxP.Control.Parameters (mkControlParameters)
-import Cardano.YTxP.Control.ParametersInitial (ControlParametersInitial)
 import Data.Aeson (eitherDecodeFileStrict)
 import Data.Aeson.Encode.Pretty (encodePretty)
 import Data.ByteString qualified as BS
@@ -21,7 +20,7 @@ main = do
           decoded <- eitherDecodeFileStrict fp
           case decoded of
             Left err -> error $ "Could not decode JSON at path " <> show fp <> "\n" <> show err
-            Right (cpi :: ControlParametersInitial Integer) -> case mkControlParameters cpi of
+            Right cpi -> case mkControlParameters cpi of
               Left err -> error $ "Could not make ControlParameters: " <> show err
               Right cp -> do
                 let encoded = BSL.toStrict . encodePretty $ cp
