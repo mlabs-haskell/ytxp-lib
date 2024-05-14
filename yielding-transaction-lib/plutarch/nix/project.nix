c8e69478ce4fc3027995dae144672f312c332890
Alberto Fanton <alberto.fanton@protonmail.com>
2024-05-14 16:29:43 +0200

    Remove useless entry in flake input and input map

--- yielding-transaction-lib/plutarch/nix/project.nix
+++ yielding-transaction-lib/plutarch/nix/project.nix
@@ -6,8 +6,6 @@ pkgs.haskell-nix.cabalProject'
   shell = import ./shell.nix { inherit pkgs; };
   inputMap = {
     "https://chap.intersectmbo.org/" = inputs.CHaP;
-    "https://github.com/Plutonomicon/plutarch-plutus.git" = inputs.plutarch;
-    "https://github.com/input-output-hk/xsy-liqwid-libs.git" = inputs.xsy-liqwid-libs;
   };
   sha256map = {
     "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg=";
