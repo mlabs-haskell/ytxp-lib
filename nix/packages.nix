9b39e4da417ba46ad9ee9acaa49d6a92f9177089
Alberto Fanton <alberto.fanton@protonmail.com>
2024-05-16 20:00:09 +0200

    Attempt to make pre-commit tools work

--- nix/packages.nix
+++ nix/packages.nix
@@ -7,7 +7,12 @@
         inputs.iohk-nix.overlays.crypto
         inputs.iohk-nix.overlays.haskell-nix-crypto
         (import ./project.nix)
-        (_: _: { inherit inputs; })
+        (_: _: {
+          inherit inputs;
+          # TODO this is not doing the expected this
+          inherit (inputs.nixpkgs-unstable) cljfmt;
+          flake = config;
+        })
       ];
       pkgs = config._module.args.pkgs.extend (lib.composeManyExtensions overlays);
       flake = pkgs.ytxp-plutarch.flake { };
