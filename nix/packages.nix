9bab5c799daf15b36da51809303405f91fe90644
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-16 15:09:16 +0200

    Refactoring nix

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,28 +1,21 @@
 { inputs, ... }:
 {
-  perSystem = { system, lib, config, ... }:
+  perSystem = { lib, config, ... }:
     let
       overlays = [
         inputs.haskellNix.overlay
         inputs.iohk-nix.overlays.crypto
         inputs.iohk-nix.overlays.haskell-nix-crypto
-        (_final: _prev: {
-          ytxp-plutarch = import ./project.nix {
-            inherit pkgs lib inputs;
-          };
-        })
+        (import ./project.nix)
+        (_: _: { inherit inputs; })
       ];
-      pkgs = import inputs.nixpkgs {
-        inherit system overlays; inherit (inputs.haskellNix) config;
-      };
+      pkgs = config._module.args.pkgs.extend (lib.composeManyExtensions overlays);
       flake = pkgs.ytxp-plutarch.flake { };
     in
     {
-      packages = {
-        ytxp-plutarch-lib = flake.packages."ytxp-plutarch:lib:ytxp-plutarch";
-        ytxp-plutarch-test-serialization = flake.packages."ytxp-plutarch:test:serialization";
-        ytxp-plutarch-write-config = flake.packages."ytxp-plutarch:exe:write-config";
+      packages = with flake.packages; {
+        inherit "ytxp-plutarch:lib:ytxp-plutarch" "ytxp-plutarch:test:serialization" "ytxp-plutarch:exe:write-config";
       };
-      devShells = flake.devShells;
+      inherit (flake) devShells;
     };
 }
--- nix/project.nix
+++ nix/project.nix
@@ -1,14 +1,14 @@
-{ pkgs, lib, inputs }:
-pkgs.haskell-nix.cabalProject'
-{
-  compiler-nix-name = "ghc96";
-  src = lib.cleanSource ./../ytxp-plutarch;
-  shell = import ./shell.nix { inherit pkgs; };
-  inputMap = {
-    "https://chap.intersectmbo.org/" = inputs.CHaP;
-  };
-  sha256map = {
-    "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg=";
-    "https://github.com/input-output-hk/xsy-liqwid-libs"."01dcaf60ee5386f0975aed908d6f92ab0e9b34e0" = "sha256-RK3yytUxIFbceU+m6MxvXyYRrWvDnhx7xmO3UcOzgKc=";
+final: prev: {
+  ytxp-plutarch = final.haskell-nix.cabalProject' {
+    compiler-nix-name = "ghc96";
+    src = final.lib.cleanSource ./../ytxp-plutarch;
+    shell = import ./shell.nix { pkgs = final; };
+    inputMap = {
+      "https://chap.intersectmbo.org/" = final.inputs.CHaP;
+    };
+    sha256map = {
+      "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg=";
+      "https://github.com/input-output-hk/xsy-liqwid-libs"."01dcaf60ee5386f0975aed908d6f92ab0e9b34e0" = "sha256-RK3yytUxIFbceU+m6MxvXyYRrWvDnhx7xmO3UcOzgKc=";
+    };
   };
 }
