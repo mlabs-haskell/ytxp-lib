8f09d5347f28adfdfe638b44bb455fcb60de5c12
Giovanni Garufi <nazrhom@gmail.com>
2024-05-21 19:31:15 +0200

    Initial attempt at updating ControlParameters

--- ytxp-plutarch/src/Cardano/YTxP/Control/Parameters.hs
+++ ytxp-plutarch/src/Cardano/YTxP/Control/Parameters.hs
@@ -3,32 +3,18 @@ Description: Data required to work with the compiled control scripts
 -}
 module Cardano.YTxP.Control.Parameters (
   -- * Types
-  YieldListScripts (
-    yieldListValidator,
-    yieldListMintingPolicy
-  ),
   YieldingScripts (
     yieldingMintingPolicies,
     yieldingValidator,
     yieldingStakingValidators
   ),
-  ControlParameters (yieldListScripts, yieldingScripts, controlParametersInitial),
+  ControlParameters (yieldingScripts, sdkParameters),
 
   -- * Parameter Generation (script compilation)
   mkControlParameters,
 ) where
 
-import Cardano.YTxP.Control.ParametersInitial (
-  ControlParametersInitial (
-    ControlParametersInitial,
-    compilationConfig,
-    maxYieldListSize,
-    mintingPoliciesNonceList,
-    scriptToWrapYieldListMP,
-    scriptToWrapYieldListValidator,
-    stakingValidatorsNonceList
-  ),
- )
+import Cardano.YTxP.Control.ParametersInitial (SdkParameters (..))
 import Cardano.YTxP.Control.YieldList.MintingPolicy (
   YieldListSTMPScript,
   compileYieldListSTMP,
@@ -62,60 +48,8 @@ import Data.Aeson (
 import Data.Text (Text)
 import Prettyprinter (Pretty (pretty), braces, punctuate, sep, (<+>))
 
-{- | Scripts that govern which transaction families can be "yielded to"
-
-@since 0.1.0
--}
-data YieldListScripts = YieldListScripts
-  { yieldListValidator :: YieldListValidatorScript
-  -- ^ The validator where we look for UTxO(s) that carry a yieldListSTT in
-  -- their values and a YieldList in their datum
-  -- @since 0.1.0
-  , yieldListMintingPolicy :: YieldListSTMPScript
-  -- ^ The minting policy for the yieldListSTT, which authenticates a
-  -- UTxO as carrying a YieldList. Note that these STTs will _usually_ be
-  -- at the yieldListValidator, but these is nothing intrinsically requiring this;
-  -- any UTxO carrying the STT will be looked at for a YieldList.
-  -- @since 0.1.0
-  }
-  deriving stock
-    ( -- | @since 0.1.0
-      Eq
-    )
-
--- | @since 0.1.0
-instance Pretty YieldListScripts where
-  {-# INLINEABLE pretty #-}
-  pretty yls =
-    ("YieldListScripts" <+>) . braces . sep . punctuate "," $
-      [ "yieldListValidator: " <+> (pretty . yieldListValidator $ yls)
-      , "yieldListMintingPolicy: " <+> (pretty . yieldListMintingPolicy $ yls)
-      ]
-
--- | @since 0.1.0
-instance ToJSON YieldListScripts where
-  {-# INLINEABLE toJSON #-}
-  toJSON yls =
-    object
-      [ "yieldListValidator" .= yieldListValidator yls
-      , "yieldListMintingPolicy" .= yieldListMintingPolicy yls
-      ]
-  {-# INLINEABLE toEncoding #-}
-  toEncoding yls =
-    pairs $
-      "yieldListValidator" .= yieldListValidator yls
-        <> "yieldListMintingPolicy" .= yieldListMintingPolicy yls
-
--- | @since 0.1.0
-instance FromJSON YieldListScripts where
-  {-# INLINEABLE parseJSON #-}
-  parseJSON = withObject "YieldListScripts" $ \obj -> do
-    ylv <- obj .: "yieldListValidator"
-    ylmp <- obj .: "yieldListMintingPolicy"
-    pure $ YieldListScripts ylv ylmp
-
-{- | Scripts that yield to transaction families described by the datums guarded
-by the YieldListScripts.
+{- | Scripts that yield to transaction families identified by reference scripts
+which carry a state thread token
 
 @since 0.1.0
 -}
@@ -127,9 +61,6 @@ data YieldingScripts = YieldingScripts
   , yieldingStakingValidators :: [YieldingStakingValidatorScript]
   -- ^ @since 0.1.0
   }
-
--- \^ We have multiple of these, because each can only be delegated to a single
--- pool.
 
 -- | @since 0.1.0
 instance ToJSON YieldingScripts where
@@ -164,11 +95,9 @@ library.
 @since 0.1.0
 -}
 data ControlParameters = ControlParameters
-  { yieldListScripts :: YieldListScripts
+  { yieldingScripts :: YieldingScripts
   -- ^ @since 0.1.0
-  , yieldingScripts :: YieldingScripts
-  -- ^ @since 0.1.0
-  , controlParametersInitial :: ControlParametersInitial
+  , sdkParameters :: SdkParameters
   -- ^ @since 0.1.0
   }
 
@@ -177,81 +106,61 @@ instance ToJSON ControlParameters where
   {-# INLINEABLE toJSON #-}
   toJSON cp =
     object
-      [ "yieldListScripts" .= yieldListScripts cp
-      , "yieldingScripts" .= yieldingScripts cp
-      , "controlParametersInitial" .= controlParametersInitial cp
+      [ "yieldingScripts" .= yieldingScripts cp
+      , "sdkParameters" .= sdkParameters cp
       ]
   {-# INLINEABLE toEncoding #-}
   toEncoding cp =
     pairs $
-      "yieldListScripts" .= yieldListScripts cp
-        <> "yieldingScripts" .= yieldingScripts cp
-        <> "controlParametersInitial" .= controlParametersInitial cp
+      "yieldingScripts" .= yieldingScripts cp
+        <> "sdkParameters" .= sdkParameters cp
 
 -- | @since 0.1.0
 instance FromJSON ControlParameters where
   {-# INLINEABLE parseJSON #-}
   parseJSON = withObject "ControlParameters" $ \obj -> do
-    yls <- obj .: "yieldListScripts"
     ys <- obj .: "yieldingScripts"
-    cpi <- obj .: "controlParametersInitial"
-    pure $ ControlParameters yls ys cpi
+    cpi <- obj .: "sdkParameters"
+    pure $ ControlParameters ys cpi
 
 {- | Compile all scripts, threading through the appropriate parameters and
 script hashes
 -}
 mkControlParameters ::
-  ControlParametersInitial ->
+  SdkParameters ->
   Either
     Text
     ControlParameters
 mkControlParameters
-  cpi@ControlParametersInitial
-    { maxYieldListSize
-    , stakingValidatorsNonceList
+  cpi@SdkParameters
+    { stakingValidatorsNonceList
     , mintingPoliciesNonceList
-    , scriptToWrapYieldListMP
-    , scriptToWrapYieldListValidator
+    , authorisedScriptsSTCS
     , compilationConfig
     } =
     do
       ------------------------------------------------------------
-      -- Compile the yield list scripts first. We need their
-      -- hashes in order to parameterize the yielding scripts.
-      yieldListSTMP <-
-        compileYieldListSTMP compilationConfig maxYieldListSize scriptToWrapYieldListMP
-      yieldListVal <-
-        compileYieldListValidator compilationConfig scriptToWrapYieldListValidator
-
-      let
-        ylstcs = mkYieldListSTCS yieldListSTMP
-
-      ------------------------------------------------------------
       -- Now compile the yielding scripts
-      yieldingVal <- compileYieldingValidator compilationConfig ylstcs
+      yieldingVal <- compileYieldingValidator compilationConfig authorisedScriptsSTCS
 
       -- Compile the staking validators, pulling any @Left@s (containing compilation
       -- error messages) through the list
       yieldingStakingVals <-
         mapM
-          (compileYieldingStakingValidator compilationConfig ylstcs)
+          (compileYieldingStakingValidator compilationConfig authorisedScriptsSTCS)
           stakingValidatorsNonceList
 
       yieldingMPs <-
         mapM
-          (compileYieldingMP compilationConfig ylstcs)
+          (compileYieldingMP compilationConfig authorisedScriptsSTCS)
           mintingPoliciesNonceList
 
       pure $
         ControlParameters
-          { yieldListScripts =
-              YieldListScripts
-                yieldListVal
-                yieldListSTMP
-          , yieldingScripts =
+          { yieldingScripts =
               YieldingScripts
                 yieldingMPs
                 yieldingVal
                 yieldingStakingVals
-          , controlParametersInitial = cpi
+          , sdkParameters = cpi
           }
