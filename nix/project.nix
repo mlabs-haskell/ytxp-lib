f24569cbe1656d892bbafdf3cdc5cea5b6267c3a
Giovanni Garufi <nazrhom@gmail.com>
2024-05-23 12:05:05 +0200

    Fix nix shell

--- nix/project.nix
+++ nix/project.nix
@@ -12,7 +12,7 @@ final: _prev: {
     sha256map = {
       "https://github.com/Plutonomicon/plutarch-plutus"."7efbb9fc9f358cfbfd89ff245d85632a17ff3fb3" = "sha256-S7UQrcBO+6G77O4OAAMfQNrChuGzfTr9ARovjAcP7eg=";
       "https://github.com/input-output-hk/xsy-liqwid-libs"."01dcaf60ee5386f0975aed908d6f92ab0e9b34e0" = "sha256-RK3yytUxIFbceU+m6MxvXyYRrWvDnhx7xmO3UcOzgKc=";
-      "https://github.com/mlabs-haskell/ytxp-sdk"."0c6b15be554cd3020a67e7c7507cecbf93e72dad" = "sha256-3hkqrH4+dghrMxEq/sCs2EnhoZQD5BxZ2tNjl34BayQ=";
+      "https://github.com/mlabs-haskell/ytxp-sdk"."dd5bc87cd47af15eff2eb394e0d80177743f70fd" = "sha256-Lyd0dTtlsiyISxkCmHGRxqW5v9L4zlnB8sX5RpRUvTk=";
     };
   };
 }
