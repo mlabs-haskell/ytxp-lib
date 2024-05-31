8a110e1d27eadc09c9ecdef85681058e8bb38d51
Giovanni Garufi <nazrhom@gmail.com>
2024-05-31 13:01:59 +0100

    Unify script compilation in a single file, change compileYieldingX signatures so we can extract the plutarch script for testing without serialising it

---
+++ ytxp-plutarch/src/Cardano/YTxP/Control/Yielding/Scripts.hs
@@ -0,0 +1,73 @@
+module Cardano.YTxP.Control.Yielding.Scripts (
+  compileYieldingMP,
+  compileYieldingValidator,
+  compileYieldingSV,
+) where
+
+import Cardano.YTxP.Control.Yielding.Helper (yieldingHelper)
+import Cardano.YTxP.SDK.SdkParameters (AuthorisedScriptsSTCS)
+import Data.Text (Text)
+import Numeric.Natural (Natural)
+import Plutarch (Config, compile)
+import Plutarch.Api.V2 (PScriptContext)
+import Plutarch.Script (Script)
+
+--------------------------------------------------------------------------------
+-- Yielding Validator Script
+
+compileYieldingValidator ::
+  Config ->
+  AuthorisedScriptsSTCS ->
+  Either
+    Text
+    Script
+compileYieldingValidator config ylstcs = do
+  let
+    yieldingValidator ::
+      forall (s :: S).
+      ( Term s (PData :--> PData :--> PScriptContext :--> POpaque)
+      )
+    -- Takes the @yieldingHelper@ and turn it into a 3 argument script
+    yieldingValidator = plam $ \_datum redeemer ctx ->
+      yieldingHelper ylstcs # redeemer # ctx
+
+  compile config yieldingValidator
+
+--------------------------------------------------------------------------------
+-- Yielding Minting Policy Script
+
+compileYieldingMP ::
+  Config ->
+  AuthorisedScriptsSTCS ->
+  Natural ->
+  Either
+    Text
+    Script
+compileYieldingMP config stcs nonce = do
+  let
+    yieldingMP ::
+      forall (s :: S).
+      ( Term s (PData :--> PScriptContext :--> POpaque)
+      )
+    yieldingMP = plet (pconstant $ toInteger nonce) (const $ yieldingHelper stcs)
+
+  compile config yieldingMP
+
+--------------------------------------------------------------------------------
+-- Yielding Staking Validator
+
+{- | Compile a yielding staking validator that has been nonced.
+The nonce is required because each staking validator can only
+be delegated to a single pool; the inclusion of the nonce will change the
+script hash.
+Since SVs and MPs share the same signature they share the same implementation,
+this function is only provided for semantic clarity
+-}
+compileYieldingSV ::
+  Config ->
+  AuthorisedScriptsSTCS ->
+  Natural ->
+  Either
+    Text
+    Script
+compileYieldingSV = compileYieldingMP
