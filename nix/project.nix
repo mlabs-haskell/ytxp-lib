fcc5525d9af2719ff86ea36ae872fa04e5d95cd4
edgr <edgr@pm.me>
2025-05-09 11:37:02 +0800

    ytxp-sdk update

--- nix/project.nix
+++ nix/project.nix
@@ -12,7 +12,7 @@ final: _prev: {
     sha256map = {
       "https://github.com/mlabs-haskell/plutus-test"."648adbd910e7e9ae0efb082bc936eb304c610ef0" = "sha256-YVHoY4Pg+v0S+2isuEpZ5Krg9o3+wQcWX9zo6DtgDb8=";
       "https://github.com/mlabs-haskell/ply"."b24c919a0cddc3a3a2ace4c88f8a185db01bca04" = "sha256-wXklqWiB/zB+97A3WcdPJfFFSqiPr+4X7EZXBYesUck=";
-      "https://github.com/mlabs-haskell/ytxp-sdk"."8aff5092a058e2b5eaa3da6dc8ec2a89bfa39e66" = "sha256-R3+1PdSmykWor87NH+THMRI5gqi2FoR6CI+j4qk4JGk=";
+      "https://github.com/mlabs-haskell/ytxp-sdk"."52d8e56f7eb347b76e60eeac273121534e8b0021" = "sha256-lcrbvqJD3J+yxafVoIQsp4hFUbqc1eWj5s2YTrSUEnw=";
       "https://github.com/Plutonomicon/plutarch-plutus"."f84a46287b06f36abf8d2d63bec7ff75d32f3e91" = "sha256-gKBk9D6DHSEudq7P9+07yXWcgM/QX7NFp0tJXBodopM=";
     };
   };
