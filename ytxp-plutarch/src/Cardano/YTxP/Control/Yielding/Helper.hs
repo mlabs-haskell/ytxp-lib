1cfd5b6de704299d186e0c895942e21b5e9a0d57
edgr <edgr@pm.me>
2025-06-06 12:28:52 +0800

    Oneshot backdor validation

--- ytxp-plutarch/src/Cardano/YTxP/Control/Yielding/Helper.hs
+++ ytxp-plutarch/src/Cardano/YTxP/Control/Yielding/Helper.hs
@@ -1,7 +1,11 @@
 {- | This module export a helper function that produces a two argument yielding script that
 we use to implement the logic for yielding validator, minting policy and staking validator
 -}
-module Cardano.YTxP.Control.Yielding.Helper (yieldingHelper) where
+module Cardano.YTxP.Control.Yielding.Helper (
+  yieldingHelper,
+  oneshotHelper,
+)
+where
 
 import Cardano.YTxP.Control.Yielding (
   PAuthorisedScriptPurpose (PMinting, PRewarding, PSpending),
@@ -14,7 +18,9 @@ import Plutarch.LedgerApi.V3 (
   PCurrencySymbol,
   PRedeemer (PRedeemer),
   PScriptContext,
+  PTxOutRef,
   paddress'credential,
+  pfindOwnInput,
   pscriptContext'redeemer,
   pscriptContext'txInfo,
   ptxInInfo'resolved,
@@ -24,7 +30,8 @@ import Plutarch.LedgerApi.V3 (
   ptxInfo'wdrl,
   ptxOut'address,
  )
-import Utils (pcheck, pscriptHashToCurrencySymbol)
+import Plutarch.Maybe (pisJust)
+import Utils (pscriptHashToCurrencySymbol)
 
 -- -   Look at the UTxO at the `n` th entry in the `txInfoReferenceInputs`, where `n` is equal to `authorisedScriptIndex`.
 --     -   Call this UTxO `authorisedScriptUTxO`.
@@ -38,7 +45,7 @@ import Utils (pcheck, pscriptHashToCurrencySymbol)
 
 yieldingHelper ::
   forall (s :: S).
-  Term s (PCurrencySymbol :--> PScriptContext :--> PUnit)
+  Term s (PCurrencySymbol :--> PScriptContext :--> PBool)
 yieldingHelper = plam $ \pylstcs ctx' -> unTermCont $ do
   ctx <- pmatchC ctx'
   redeemer' <- pletC $ pscriptContext'redeemer ctx
@@ -54,39 +61,51 @@ yieldingHelper = plam $ \pylstcs ctx' -> unTermCont $ do
       authorisedScriptPurpose = pfromData $ pfstBuiltin # scriptProofIndex
       authorisedScriptIndex = pfromData $ psndBuiltin # scriptProofIndex
 
-  pure $
-    pcheck $
-      pmatch authorisedScriptPurpose $ \case
-        PMinting ->
-          let txInfoMints = pfromData $ ptxInfo'mint txInfo
-              authorisedScriptMint = pto (pto txInfoMints) #!! authorisedScriptIndex
-              currencySymbol = pscriptHashToCurrencySymbol authorisedScriptHash
-           in ptraceInfoIfFalse
-                "Minting policy does not match expected authorised minting policy"
-                $ pfromData (pfstBuiltin # authorisedScriptMint) #== currencySymbol
-        PSpending -> unTermCont $ do
-          let txInfoInputs = pfromData $ ptxInfo'inputs txInfo
-          authorisedScriptInput <-
-            pmatchC $ pfromData $ txInfoInputs #!! authorisedScriptIndex
-          out <- pmatchC $ ptxInInfo'resolved authorisedScriptInput
-          address <- pmatchC $ ptxOut'address out
-          let credential = paddress'credential address
-          return $
-            pmatch credential $ \case
-              PScriptCredential hash ->
-                ptraceInfoIfFalse "Input does not match expected authorised validator" $
-                  pfromData hash #== authorisedScriptHash
-              PPubKeyCredential _ ->
-                ptraceInfoError "Input at specified index is not a script input"
-        PRewarding -> unTermCont $ do
-          PMap txInfoWithdrawals <- pmatchC $ pfromData $ ptxInfo'wdrl txInfo
-          let authorisedScriptWithdrawal = txInfoWithdrawals #!! authorisedScriptIndex
-          return $
-            pmatch (pfromData $ pfstBuiltin # authorisedScriptWithdrawal) $ \case
-              PScriptCredential hash ->
-                ptraceInfoIfFalse
-                  "Withdrawal does not match expected authorised staking validator"
-                  $ pfromData hash #== authorisedScriptHash
-              PPubKeyCredential _ ->
-                ptraceInfoError
-                  "Staking credential at specified index is not a script credential"
+  pure . pmatch authorisedScriptPurpose $ \case
+    PMinting ->
+      let txInfoMints = pfromData $ ptxInfo'mint txInfo
+          authorisedScriptMint = pto (pto txInfoMints) #!! authorisedScriptIndex
+          currencySymbol = pscriptHashToCurrencySymbol authorisedScriptHash
+       in ptraceInfoIfFalse
+            "Minting policy does not match expected authorised minting policy"
+            $ pfromData (pfstBuiltin # authorisedScriptMint) #== currencySymbol
+    PSpending -> unTermCont $ do
+      let txInfoInputs = pfromData $ ptxInfo'inputs txInfo
+      authorisedScriptInput <-
+        pmatchC $ pfromData $ txInfoInputs #!! authorisedScriptIndex
+      out <- pmatchC $ ptxInInfo'resolved authorisedScriptInput
+      address <- pmatchC $ ptxOut'address out
+      let credential = paddress'credential address
+      return $
+        pmatch credential $ \case
+          PScriptCredential hash ->
+            ptraceInfoIfFalse "Input does not match expected authorised validator" $
+              pfromData hash #== authorisedScriptHash
+          PPubKeyCredential _ ->
+            ptraceInfoError "Input at specified index is not a script input"
+    PRewarding -> unTermCont $ do
+      PMap txInfoWithdrawals <- pmatchC $ pfromData $ ptxInfo'wdrl txInfo
+      let authorisedScriptWithdrawal = txInfoWithdrawals #!! authorisedScriptIndex
+      return $
+        pmatch (pfromData $ pfstBuiltin # authorisedScriptWithdrawal) $ \case
+          PScriptCredential hash ->
+            ptraceInfoIfFalse
+              "Withdrawal does not match expected authorised staking validator"
+              $ pfromData hash #== authorisedScriptHash
+          PPubKeyCredential _ ->
+            ptraceInfoError
+              "Staking credential at specified index is not a script credential"
+
+-- | One-Shot check
+oneshotHelper ::
+  forall (s :: S).
+  Term
+    s
+    ( PTxOutRef :--> PScriptContext :--> PBool
+    )
+oneshotHelper = plam $ \oref ctx -> pmatch ctx $
+  \ctx' -> pmatch (pscriptContext'txInfo ctx') $
+    \txInfo ->
+      -- FIXME: There is no list fusion atm.
+      let inputs = pmap # plam pfromData # pfromData (ptxInfo'inputs txInfo)
+       in pisJust #$ pfindOwnInput # inputs # oref
--- ytxp-plutarch/src/Cardano/YTxP/Control/Yielding/Scripts.hs
+++ ytxp-plutarch/src/Cardano/YTxP/Control/Yielding/Scripts.hs
@@ -1,9 +1,19 @@
 module Cardano.YTxP.Control.Yielding.Scripts (
   yielding,
+  yielding',
 ) where
 
-import Cardano.YTxP.Control.Yielding.Helper (yieldingHelper)
-import Plutarch.LedgerApi.V3 (PCurrencySymbol, PScriptContext)
+import Cardano.YTxP.Control.Yielding.Helper (
+  oneshotHelper,
+  yieldingHelper,
+ )
+import Plutarch.Builtin.Unit (punit)
+import Plutarch.LedgerApi.V3 (
+  PCurrencySymbol,
+  PScriptContext,
+  PTxOutRef,
+ )
+import Utils (pcheck)
 
 --------------------------------------------------------------------------------
 -- Plutarch level terms
@@ -16,4 +26,21 @@ yielding ::
     ( PCurrencySymbol :--> PAsData PInteger :--> PScriptContext :--> PUnit
     )
 yielding = plam $ \psymbol _nonce ctx ->
-  yieldingHelper # psymbol # ctx
+  pcheck $ yieldingHelper # psymbol # ctx
+
+-- | Yielding Validator with one shot backdoor
+yielding' ::
+  forall (s :: S).
+  Term
+    s
+    ( PTxOutRef
+        :--> PCurrencySymbol
+        :--> PAsData PInteger
+        :--> PScriptContext
+        :--> PUnit
+    )
+yielding' = plam $ \oref psymbol _nonce ctx ->
+  pif
+    (yieldingHelper # psymbol # ctx)
+    punit
+    (pcheck $ oneshotHelper # oref # ctx)
