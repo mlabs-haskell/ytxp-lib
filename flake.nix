d261ef1c7c06ee3c3cf4b32a984ed18dd2aed7a5
edgr <edgr@pm.me>
2025-10-29 08:28:30 +0800

    Add ytxp-sdk to nix checks and docs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Yielding Transaction Pattern Library";
   inputs = {
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskellNix.url = "github:input-output-hk/haskell.nix";
     iohk-nix.url = "github:input-output-hk/iohk-nix";
@@ -10,6 +11,7 @@
       flake = false;
     };
     git-hooks.url = "github:cachix/git-hooks.nix";
+    flake-root.url = "github:srid/flake-root";
     hercules-ci-effects = {
       url = "github:mlabs-haskell/hercules-ci-effects/push-cache-effect";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,6 +23,8 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
       imports = [
+        inputs.treefmt-nix.flakeModule
+        ./nix/treefmt.nix
         ./nix/pre-commit.nix
         ./nix/packages.nix
         ./nix/checks.nix
